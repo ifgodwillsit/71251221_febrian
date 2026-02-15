034ae2fcfa39b85bfdadbd43f6eb6b8af944d87d
febrian <r.fkr147@gmail.com>
2026-02-15 21:46:31 +0700

    Delete minggu02 directory

--- minggu02/soal01.py
+++
@@ -1,9 +0,0 @@
-def hitung_bb(bmi, tinggi) -> float:
-    return(bmi * tinggi**2)
-
-bmi = float(input("Masukkan bmi: "))
-tinggi = float(input("Masukkan tinggi badan dalam m: "))
-
-bb = hitung_bb(bmi, tinggi)
-print(f"Berat badan anda adalah: {bb:.1f} kg")
-
