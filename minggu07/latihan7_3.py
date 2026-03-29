c05e318723c42e00ced5648ab3a29ecce125525e
febrian <r.fkr147@gmail.com>
2026-03-29 14:06:23 +0700

    Update latihan7_3.py

--- minggu07/latihan7_3.py
+++ minggu07/latihan7_3.py
@@ -1,9 +1,11 @@
+def persegi(tinggi, lebar):
+    for i in range(tinggi):
+        for j in range(1, lebar + 1):
+            angka = i * lebar + j
+            print(angka, end=" ")
+        print()
+    
 tinggi = int(input("Masukkan tinggi: "))
 lebar  = int(input("Masukkan lebar : "))
-
-for i in range(tinggi):
-    for j in range(1, lebar + 1):
-        angka = i * lebar + j
-        print(angka, end=" ")
-    print()
+persegi(tinggi, lebar)
     
